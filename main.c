a61fff8ddc4ce82663c31b262674992e214e8455
Rehten <Mroow@yandex.ru>
2019-06-06 20:53:25 +0300

    part01_01

--- main.c
+++ main.c
@@ -9,6 +9,12 @@ int main()
     printf("enter mass: ");
     scanf("%lf", &human_mass);
     printf("\n");
-    printf("human mass is: %.2f ", human_mass);
+    printf("human mass is: %.2f \n", human_mass);
+    printf("enter height: ");
+    scanf("%lf", &human_height);
+    printf("\n");
+    printf("human height is: %.2lf ", human_height);
+
+    printf("human mass index is : %.2lf", (human_mass/(human_height * human_height)));
     return 0;
 }
