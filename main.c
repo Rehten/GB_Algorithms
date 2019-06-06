2231b498fdbc1f75531f0c9de22ebafc98dc886a
Rehten <Mroow@yandex.ru>
2019-06-06 20:48:34 +0300

    part01_01

--- main.c
+++ main.c
@@ -2,6 +2,13 @@
 
 int main()
 {
-    printf("Hello, World!\n");
+    double human_mass;
+    double human_height;
+
+    printf("Welcome to my mass index calculator for human body!\n");
+    printf("enter mass: ");
+    scanf("%lf", &human_mass);
+    printf("\n");
+    printf("human mass is: %.2f ", human_mass);
     return 0;
 }
