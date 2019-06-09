721e333a4e00a9f1603dd54d270aae2e9824392a
Rehten <Mroow@yandex.ru>
2019-06-09 22:05:25 +0300

    part01_14

--- main.c
+++ main.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-int rand_with_crand ()
-{
-    return (rand() * time(NULL))%100;
-}
-
-int rand_without_crand ()
-{
-    int a, b;
-    b = time(NULL)%10;
-    a = time(NULL)%10 * time(NULL)%10;
-
-    return (a * (time(NULL)%100 + time(NULL)%2) + b) % 100;
-}
 
 int main(int argc, char *argv[])
 {
-    printf("%d, %d", rand_with_crand(), rand_without_crand());
+    int max;
+    printf("Enter you number: \n");
+    scanf("%d", &max);
+
+    for (int i = 1; i * i <= max; i++)
+    {
+        // проверка на автоморфность
+        // определение числа цифр в числе
+        int i_square = i * i;
+        int is_sliced = 0;
+        int slice_index = 1;
+        while ((i_square > i) || (is_sliced == 0))
+        {
+            slice_index *= 10;
+            i_square /= 10;
+            is_sliced = 1;
+        }
+
+
+        if (i == (i * i)%slice_index)
+        {
+            printf("number %d is authomorphic. Its square is %d\n", i, i * i);
+        }
+    }
 
     return 0;
 }
