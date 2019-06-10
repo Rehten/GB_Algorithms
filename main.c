3d6bc4ec7f2b8f86d874ebf32a21aa9cc78f1dc8
Rehten <Mroow@yandex.ru>
2019-06-11 00:50:28 +0300

    part02_03 - middle result

--- main.c
+++ main.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+enum Actions
+{
+    AddOne = 5,
+    MultiplyByTwo = 6
+};
 
 int power_without_recursion(int a, int b)
 {
@@ -19,49 +26,111 @@ int power_without_recursion(int a, int b)
     }
 }
 
-int power_recursion(int a, int b)
+void calculator_way_array ()
 {
-    if (b != 0)
+    int base = 3;
+    int succeed_solutions_number = 0;
+    int arr_length = 20 - base;
+    int* arr = (int *)calloc((size_t)arr_length, sizeof(int));
+    int solutions_count = power_without_recursion(2/* количество типов в enum */, arr_length);
+
+    /* тестируем, что будет при каждом экшене. В случае корректного решения записываем его в таблицу */
+    while (solutions_count != 0)
     {
-        return a * (b != 1 ? power_recursion(a, b - 1) : 1);
+        int try_number = solutions_count;
+        int iter = 0;
+        while (try_number != 0)
+        {
+            arr[iter] = try_number%2 + 5 /* чтобы проверить заполненные поля (незаполненные не могут быть выше 1 */;
+            if (try_number > 1)
+            {
+                try_number /= 2;
+            }
+            else
+            {
+                try_number -= 1;
+            }
+            iter++;
+        }
+
+        iter = 0;
+
+        for (int i = 0; i < arr_length; i++)
+        {
+            if (base < 20)
+            {
+                switch (arr[i])
+                {
+                    case AddOne:
+                        base += 1;
+                        break;
+                    case MultiplyByTwo:
+                        base *= 2;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (base >= 20)
+            {
+                for (int j = 0; j < arr_length; j++)
+                {
+                    if (j >= i)
+                    {
+                        arr[j] = 0;
+                    }
+                }
+                goto Label;
+            }
+        }
+
+        Label:
+
+        if (base == 20)
+        {
+            succeed_solutions_number++;
+            printf("%d succeed solution! base is: %d, actions list: ", succeed_solutions_number, base);
+
+            for (int i = 0; i < arr_length; i++)
+            {
+                if (arr[i] >= 5)
+                {
+                    printf(", %d - ", i);
+                    if (arr[i] == AddOne)
+                    {
+                        printf("+1");
+                    }
+                    else if (arr[i] == MultiplyByTwo)
+                    {
+                        printf("x2");
+                    }
+                }
+            }
+            printf("\n");
+
+        }
+
+        for (int i = 0; i < arr_length; i++)
+        {
+            arr[i] = 0;
+        }
+
+        base = 3;
+
+        solutions_count--;
     }
-    else
-    {
-        return 1;
-    }
+
+    free(arr);
 }
 
-int power_recursion_optimized(int a, int b)
+void calculator_way_recursion ()
 {
-    if (b > 1)
-    {
-        int c = power_recursion_optimized(a, b/2);
-        return (c * c * (b%2 == 0 ? 1 : a));
-    }
-    else if (b == 1)
-    {
-        return a;
-    }
-    else if (b == 0)
-    {
-        return 1;
-    }
+
 }
 
 int main(int argc, char *argv[])
 {
-    int a;
-    int b;
-
-    printf("Multiply a * a, b times\n");
-    printf("enter a: ");
-    scanf("%d", &a);
-    printf("enter b: ");
-    scanf("%d", &b);
-
-    printf("result without recursion is: %d\n", power_without_recursion(a, b));
-    printf("result with recursion is: %d\n", power_recursion(a, b));
-    printf("result with recursion is: %d\n", power_recursion_optimized(a, b));
-
+    calculator_way_array();
+    calculator_way_recursion();
     return 0;
 }
