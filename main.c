b47db94fc5297849b4ba38a8094e37be003b9f82
Rehten <Mroow@yandex.ru>
2019-06-10 22:54:10 +0300

    part02_01 - binary print

--- main.c
+++ main.c
@@ -1,31 +1,25 @@
 #include <stdio.h>
 
+void binary_print(int decimal)
+{
+    if (decimal > 1)
+    {
+        binary_print(decimal/2);
+        printf("%d", decimal%2);
+    } else {
+        printf("%d", decimal%2);
+    }
+}
+
 int main(int argc, char *argv[])
 {
-    int max;
-    printf("Enter you number: \n");
-    scanf("%d", &max);
+    int decimal;
 
-    for (int i = 1; i * i <= max; i++)
-    {
-        // проверка на автоморфность
-        // определение числа цифр в числе
-        int i_square = i * i;
-        int is_sliced = 0;
-        int slice_index = 1;
-        while ((i_square > i) || (is_sliced == 0))
-        {
-            slice_index *= 10;
-            i_square /= 10;
-            is_sliced = 1;
-        }
+    printf("Enter decimal number. Program convert it to binary\n");
+    scanf("%d", &decimal);
+    printf("Binary is: ");
 
-
-        if (i == (i * i)%slice_index)
-        {
-            printf("number %d is authomorphic. Its square is %d\n", i, i * i);
-        }
-    }
+    binary_print(decimal);
 
     return 0;
 }
