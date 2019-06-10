b2f83e6e4403cebd2eafb500cc68253472149913
Rehten <Mroow@yandex.ru>
2019-06-11 02:30:35 +0300

    part02_03 - solution with recursion

--- main.c
+++ main.c
@@ -7,174 +7,39 @@ enum Actions
     MultiplyByTwo = 6
 };
 
-int power_without_recursion(int a, int b)
+int calculator_way_recursion (int base, int max, int* result_count)
 {
-    if (b != 0)
+    if (base < max)
     {
-        int result = 1;
-        while (b > 0)
+        int plus_one = calculator_way_recursion(base + 1, max, result_count);
+        if (plus_one == 1)
         {
-            result *= a;
-            b--;
+            *result_count = *result_count + 1;
         }
 
-        return result;
+        int multiply_two = calculator_way_recursion(base * 2, max, result_count);
+        if (multiply_two == 1)
+        {
+            *result_count = *result_count + 1;
+        }
+
+        return 0;
     }
-    else
+    else if (base == max)
     {
         return 1;
     }
-}
-
-void calculator_way_array ()
-{
-    int base = 3;
-    int succeed_solutions_number = 0;
-    int arr_length = 20 - base;
-    int* arr = (int *)calloc((size_t)arr_length, sizeof(int));
-    int solutions_count = power_without_recursion(2/* количество типов в enum */, arr_length);
-    int** solutions = (int**)calloc((size_t)solutions_count, sizeof(int*));
-
-    for (int i = 0; i < solutions_count; i++)
+    else
     {
-        solutions[i] = NULL;
+        return -1;
     }
-
-    /* тестируем, что будет при каждом экшене. В случае корректного решения записываем его в таблицу */
-    while (solutions_count != 0)
-    {
-        int try_number = solutions_count;
-        int iter = 0;
-        while (try_number != 0)
-        {
-            arr[iter] = try_number%2 + 5 /* чтобы проверить заполненные поля (незаполненные не могут быть выше 1 */;
-            if (try_number > 1)
-            {
-                try_number /= 2;
-            }
-            else
-            {
-                try_number -= 1;
-            }
-            iter++;
-        }
-
-        iter = 0;
-
-        for (int i = 0; i < arr_length; i++)
-        {
-            if (base < 20)
-            {
-                switch (arr[i])
-                {
-                    case AddOne:
-                        base += 1;
-                        break;
-                    case MultiplyByTwo:
-                        base *= 2;
-                        break;
-                    default:
-                        break;
-                }
-            }
-            else if (base >= 20)
-            {
-                for (int j = 0; j < arr_length; j++)
-                {
-                    if (j >= i)
-                    {
-                        arr[j] = 0;
-                    }
-                }
-                goto Label;
-            }
-        }
-
-        Label:
-
-        if (base == 20)
-        {
-            int* solution_copy = (int *)calloc((size_t)arr_length, sizeof(int));
-
-            for (int i = 0; i < arr_length; i++)
-            {
-                solution_copy[i] = arr[i];
-            }
-
-            int is_arrays_equals = 0;
-            int ready_solutions = 0;
-            int ready_unique_solutions = 0;
-
-            for (int i = 0; i < solutions_count; i++)
-            {
-                if (solutions[i] != NULL)
-                {
-                    int is_equals = 1;
-
-                    for (int j = 0; j < arr_length; j++)
-                    {
-                        if (solutions[i][j] != solution_copy[j])
-                        {
-                            is_equals = 0;
-                        };
-                    }
-                    ready_solutions++;
-                    if (is_equals == 0)
-                    {
-                        ready_unique_solutions++;
-                    }
-                }
-            }
-
-            is_arrays_equals = ready_solutions - ready_unique_solutions;
-
-            if (is_arrays_equals == 0)
-            {
-                solutions[succeed_solutions_number] = solution_copy;
-
-                succeed_solutions_number++;
-                printf("%d succeed solution! base is: %d, actions list: ", succeed_solutions_number, base);
-                for (int i = 0; i < arr_length; i++)
-                {
-                    if (arr[i] >= 5)
-                    {
-                        printf(", %d - ", i + 1);
-                        if (arr[i] == AddOne)
-                        {
-                            printf("+1");
-                        }
-                        else if (arr[i] == MultiplyByTwo)
-                        {
-                            printf("x2");
-                        }
-                    }
-                }
-                printf("\n");
-
-            }
-        }
-
-        for (int i = 0; i < arr_length; i++)
-        {
-            arr[i] = 0;
-        }
-
-        base = 3;
-
-        solutions_count--;
-    }
-
-    free(arr);
-}
-
-void calculator_way_recursion ()
-{
 
 }
 
 int main(int argc, char *argv[])
 {
-    calculator_way_array();
-    calculator_way_recursion();
+    int count = 0;
+    calculator_way_recursion(3, 20, &count);
+    printf("Total solutions is %d", count);
     return 0;
 }
