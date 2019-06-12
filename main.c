42059eae7d6ef472ce87af6c67caade58a7b4756
Rehten <Mroow@yandex.ru>
2019-06-12 21:32:39 +0300

    part03_03 - boilerplate code - refactoring

--- main.c
+++ main.c
@@ -1,26 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int binary_find (int val, int arr[])
+struct Result
 {
-    int rslt[2] = {0, 0};
+    int rslt_val;
+    int rslt_index;
+};
 
-    return *rslt;
+struct Result binary_find (int val, int arr[])
+{
+    struct Result rslt = {0, 0};
+
+    return rslt;
 }
 
-void print_binary_find (int* rslt)
+void print_binary_find (struct Result rslt)
 {
-    printf("\nResult of Binary finding: { %d, %d, };", rslt[0], rslt[1]);
+    printf("\nResult of Binary finding: { rslt_val: %d, rslt_index: %d };", rslt.rslt_val, rslt.rslt_index);
 }
 
 int main(int argc, char *argv[])
 {
-    int target_array[25];
+    int arr_size;
+    int arr_multiply;
+    printf("Enter arr size: ");
+    scanf("%d", &arr_size);
+    printf("Enter arr member multiply constant(for 3 - arr[1] == 3, arr[2] == 6): ");
+    scanf("%d", &arr_multiply);
+    int target_array[arr_size];
 
     printf("Array: { ");
     for (int i = 0; i < sizeof(target_array) / sizeof(target_array[0]); i++)
     {
-        target_array[i] = i * 3;
+        target_array[i] = (i + 1) * arr_multiply;
 
         if (i != 0)
         {
@@ -30,8 +42,8 @@ int main(int argc, char *argv[])
     }
     printf(" }");
 
-    int find_rslt = binary_find(1, target_array);
+    struct Result find_rslt = binary_find(1, target_array);
 
-    print_binary_find(&find_rslt);
+    print_binary_find(find_rslt);
     return 0;
 }
