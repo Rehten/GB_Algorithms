3fb07552e987bc07108a6e5014d126727c803619
Rehten <Mroow@yandex.ru>
2019-06-20 00:57:45 +0300

    part05_03 - boilerplate code

--- main.c
+++ main.c
@@ -1,30 +1,151 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
-// Сысоев - Перевод десятичной в двоичную на стеке
+// ВАЖНО!!! ПРОЧИТАТЬ ВЕСЬ КОММЕНТАРИЙ!!!
+// Сысоев - часть 05 - вычислитель выражений (задания 03 + 05)
+// суть следующая - нужно ввести строку(без пробелов и табов), начинающуюся с любых скобок (например (1+2)) - и вычислитель должен выдать 3
+// для хардкора используются три вида скобок, причем [1+2] = 3*3 = 9, а {1+2} = 3+3 = 6;
+// таким образом выражение {1+[2+(3+4)]} -> {1+[2+7]} -> {1+9+9} -> {19} -> 19+19 -> 38
+// НЕЛЬЗЯ ВВОДИТЬ ПРОБЕЛЫ!!!p
 struct StackFrame {
-    int num[5000];
+    int num;
     struct StackFrame* next;
 };
 
-struct StackFrame* next_frame(struct StackFrame* prev)
+struct BracketStackFrame {
+    // хранит открывающие скобки. При встрече в строке закрывающей скобки, один стековый кадр должен освободиться
+    char bracket;
+    struct BracketStackFrame* next;
+};
+
+struct BracketStackFrame* next_bracket(struct BracketStackFrame* prev)
 {
-    prev->next = malloc(sizeof(struct StackFrame));
+    prev->next = malloc(sizeof(struct BracketStackFrame));
+    prev->next->next = NULL;
     return prev->next;
+}
+
+void clear_frame(struct BracketStackFrame* frame)
+{
+    free(frame);
+}
+
+char cur_open_brace(char open_brace)
+{
+    switch (open_brace)
+    {
+        case '{':
+            return '{';
+        case '[':
+            return '[';
+        case '(':
+            return '(';
+        default:
+            return 'o';
+    }
+}
+
+char next_closed_brace(char open_brace)
+{
+    switch (open_brace)
+    {
+        case '{':
+            return '}';
+        case '[':
+            return ']';
+        case '(':
+            return ')';
+        default:
+            return 'c';
+    }
+}
+
+char prev_open_brace(char closed_brace)
+{
+    switch (closed_brace)
+    {
+        case '{':
+            return '{';
+        case ']':
+            return '[';
+        case ')':
+            return '(';
+        default:
+            return 'o';
+    }
+}
+
+void print_zstring(char* string, int length)
+{
+    for (int i = 0; i < length; i++)
+    {
+        if (string[i] == 'z')
+        {
+            string[i] = '\0';
+        }
+    }
+
+    printf("%s", string);
+}
+
+void is_string_valid(char* string, int length)
+{
+    int is_valid = 1;
+    struct BracketStackFrame* first = calloc(sizeof(struct BracketStackFrame), 1);
+    first->bracket = string[0];
+    first->next = NULL;
+    struct BracketStackFrame* buffer = first;
+    char next_closed_bracket = next_closed_brace(first->bracket);
+
+    for (int i = 0; i < length; i++)
+    {
+        if (cur_open_brace(string[i]) != 'o')
+        {
+            buffer = next_bracket(buffer);
+            buffer->bracket = cur_open_brace(string[i]);
+            next_closed_bracket = next_closed_brace(string[i]);
+            printf("\n next open is %c, and closed is %c", buffer->bracket, next_closed_bracket);
+        }
+        if (string[i] == 'z')
+        {
+            break;
+        }
+    }
+
+
+    if (is_valid)
+    {
+        printf("\nString is valid");
+    }
+    else
+    {
+        printf("\nERROR!!! String is invalid");
+    }
 }
 
 int main(int argc, char *argv[])
 {
-    struct StackFrame* current = malloc(sizeof(struct StackFrame));
-
-    while (current != NULL)
+    int length = 5000;
+    char* user_expression = calloc(sizeof(char), (size_t)length);
+    for (int i = 0; i < length; i++)
     {
-        current = next_frame(current);
-        printf("\nCurrent is %p", current);
+        if (i + 1 != length)
+        {
+            user_expression[i] ='z';
+        }
+        else
+        {
+            user_expression[i] ='\0';
+        }
     }
 
-    printf("Error, too much memory allocated");
+    printf("Enter your expression: ");
+    scanf("%s", user_expression);
+
+    is_string_valid(user_expression, length);
+
+    printf("\nYou entered: ");
+    print_zstring(user_expression, length);
 
     return 0;
 }
