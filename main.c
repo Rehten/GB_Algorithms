96926ba52fd169e5d3b73d6e37f847fa505de538
Rehten <Mroow@yandex.ru>
2019-06-27 23:18:24 +0300

    part08_01 - complete

--- main.c
+++ main.c
@@ -4,6 +4,26 @@
 // Сысоев - Считывание матрицы смежности с файла
 int main(int argc, char *argv[])
 {
-    printf("Hello, World!");
+    char ch, file_name[25];
+    FILE *fp;
+
+    printf("Enter name of a file you wish to see\n");
+    gets(file_name);
+
+    fp = fopen(file_name, "r"); // read mode
+
+    if (fp == NULL)
+    {
+        perror("Error while opening the file.\n");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("The contents of %s file are:\n", file_name);
+
+    while((ch = fgetc(fp)) != EOF)
+        printf("%c", ch);
+
+    fclose(fp);
+    return 0;
     return 0;
 }
